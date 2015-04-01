4b7772abf9430cd41ce4881b9e5ab4556ea7f902
mdhoskins <tud45816@temple.edu>
2015-04-01 17:54:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 //app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendFile('/index.html');
+	res.sendFile('./index.html');
 });
 
 app.listen(app.get('port'), function() {
